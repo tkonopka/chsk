15d10734e42a4866d0eec51146785c7eab01bb0c
tkonopka <tokonopka@gmail.com>
2022-09-21 15:13:16 +0100

    attempt at unit test for Counter

--- packages/core/tests/typography.test.tsx
+++ packages/core/tests/typography.test.tsx
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react'
-import { Chart, Typography, Label } from '../src'
+import { fireEvent, render, screen } from '@testing-library/react'
+import { Chart, Typography, Label, Counter } from '../src'
 import { chartProps } from './props'
 import { getTransform } from './utils'
 
@@ -139,5 +139,32 @@ describe('Label', () => {
         )
         const label = screen.queryByRole('label')
         expect(label).toBeNull()
+    })
+})
+
+describe('Counter', () => {
+    it('creates a default counter component', () => {
+        render(
+            <Chart {...chartProps}>
+                <Counter>50</Counter>
+            </Chart>
+        )
+        expect(screen.getByRole('counter')).toBeDefined()
+        const result = screen.getByRole('chart-content').querySelectorAll('text')
+        expect(result).toHaveLength(1)
+    })
+
+    it('updates values', () => {
+        render(
+            <Chart {...chartProps}>
+                <Counter>50</Counter>
+            </Chart>
+        )
+        const counter = screen.getByRole('counter')
+        expect(counter).toBeDefined()
+        // this is an attempt to trigger the counter animation,
+        // but the animation code is not covered... this needs improvement
+        fireEvent.change(counter, { target: { textContent: 60 } })
+        expect(counter.textContent).toEqual('60')
     })
 })
