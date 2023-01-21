e60573dc9ba0ee4fe1332f68038e84cdaae042d8
tkonopka <tokonopka@gmail.com>
2023-01-21 11:06:09 +0000

    tweeks to time series line chart

--- examples/line/TimeSeriesChart.tsx
+++ examples/line/TimeSeriesChart.tsx
@@ -1,7 +1,20 @@
-import { Chart, Axis, AxisLabel, AxisTicks, GridLines, Typography } from '@chsk/core'
-import { isScatterData, Scatter, ScatterCurve } from '@chsk/xy'
+import {
+    Chart,
+    Axis,
+    AxisLabel,
+    AxisTicks,
+    GridLines,
+    Typography,
+    SymbolProps,
+    useScales,
+    Circle,
+    addColor,
+} from '@chsk/core'
+import { isScatterData, Scatter, ScatterCurve, useScatterPreparedData } from '@chsk/xy'
+import { downloadThemePiece } from '@chsk/themes'
 import { generateRandomWalk } from './generators'
 import { MilestoneStory } from '../types'
+import { DownloadButtons } from '../navigation'
 
 const dayLength = 1000 * 60 * 60 * 24
 const aYearAgo = new Date(Date.now() - 365 * dayLength)
@@ -25,6 +38,24 @@ const dateToString = (d: Date) => {
     return result
 }
 
+const FirstPoint = ({
+    id,
+    r = 5,
+    className,
+    style,
+    setRole,
+}: Omit<SymbolProps, 'cx' | 'cy'> & { id?: string }) => {
+    const scales = useScales()
+    const preparedData = useScatterPreparedData()
+    const index = id ? preparedData.seriesIndexes[id] ?? 0 : 0
+    const cx = preparedData.data[index].x[0]
+    const cy = preparedData.data[index].y[0]
+    const pointStyle = addColor(style, scales.color(index))
+    return (
+        <Circle cx={cx} cy={cy} r={r} setRole={setRole} style={pointStyle} className={className} />
+    )
+}
+
 export const TimeSeriesChart = ({ fref, chartData, rawData }: MilestoneStory) => {
     if (!isScatterData(rawData)) return null
     return (
@@ -33,7 +64,8 @@ export const TimeSeriesChart = ({ fref, chartData, rawData }: MilestoneStory) =>
             fref={fref}
             id="time-series"
             size={[600, 400]}
-            padding={[80, 40, 60, 60]}
+            padding={[80, 40, 60, 80]}
+            theme={downloadThemePiece}
         >
             <Scatter
                 data={rawData}
@@ -50,6 +82,7 @@ export const TimeSeriesChart = ({ fref, chartData, rawData }: MilestoneStory) =>
                 }}
             >
                 <GridLines variant={'y'} style={{ stroke: '#cccccc' }} />
+                <GridLines variant={'y'} values={[0]} style={{ stroke: '#222222' }} />
                 <Axis variant={'bottom'} label={'time'}>
                     <AxisTicks
                         variant={'bottom'}
@@ -57,16 +90,23 @@ export const TimeSeriesChart = ({ fref, chartData, rawData }: MilestoneStory) =>
                     />
                 </Axis>
                 <Axis variant={'left'}>
-                    <AxisLabel variant={'left'}>Value (a.u.)</AxisLabel>
+                    <AxisLabel variant={'left'} offset={60}>
+                        Value (a.u.)
+                    </AxisLabel>
                     <AxisTicks variant={'left'} labelFormat={v => Number(v).toFixed(1)} />
                 </Axis>
-                <ScatterCurve curve={'Natural'} style={{ stroke: '#ff0000', strokeWidth: 2 }} />
-                <Typography variant={'title'} position={[-50, -50]}>
+                <ScatterCurve
+                    curve={'Linear'}
+                    style={{ stroke: '#ff0000', strokeWidth: 2, fillOpacity: 0 }}
+                />
+                <FirstPoint style={{ fill: '#222222' }} />
+                <Typography variant={'title'} position={[-65, -50]}>
                     Change over one year
                 </Typography>
-                <Typography variant={'subtitle'} position={[-50, -28]}>
+                <Typography variant={'subtitle'} position={[-65, -28]}>
                     Base values set to zero for {dateToString(aYearAgo)}
                 </Typography>
+                <DownloadButtons position={[390, -48]} data image />
             </Scatter>
         </Chart>
     )
