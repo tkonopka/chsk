c7398fc174ec6e506ad13b56a728949bc6d2546d
tkonopka <tokonopka@gmail.com>
2023-03-15 16:37:33 +0000

    fix style bug in chart with many lines

--- examples/line/ManyRandomWalksChart.tsx
+++ examples/line/ManyRandomWalksChart.tsx
@@ -37,6 +37,9 @@ const customTheme: ThemeSpec = {
         legendItem: {
             fillOpacity: 0,
         },
+        legendTitle: {
+            fillOpacity: 0,
+        },
     },
     path: {
         default: {
