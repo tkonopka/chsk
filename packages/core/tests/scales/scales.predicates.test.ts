59cc6a0b6058528d227cec5bd9542bf0b3620dbb
tkonopka <tokonopka@gmail.com>
2023-03-23 11:28:45 +0000

    fix file-name error in tests

--- packages/core/tests/scales/scales.predicates.test.ts
+++ packages/core/tests/scales/scales.predicates.test.ts
@@ -1,39 +1,39 @@
 import { isScaleWithDomain } from '../../src'
 
 describe('isScaleWithDomain', () => {
-    it('checks band scale with domain', () => {
+    it('validates band scale with domain', () => {
         const result = isScaleWithDomain({ variant: 'band', domain: ['a', 'b'] })
         expect(result).toBeTruthy()
     })
 
-    it('checks band scale with auto domain', () => {
+    it('detects domain is auto and needs filling in', () => {
         const result = isScaleWithDomain({ variant: 'band', domain: 'auto' })
         expect(result).toBeFalsy()
     })
 
-    it('checks band scale without domain', () => {
+    it('detects domain is empty', () => {
         const result = isScaleWithDomain({ variant: 'band' })
         expect(result).toBeFalsy()
     })
 
-    it('checks linear scale with domain', () => {
+    it('validates linear scale with domain', () => {
         const result = isScaleWithDomain({ variant: 'linear', domain: [0, 20] })
         expect(result).toBeTruthy()
     })
 
-    it('checks linear scale with auto domain', () => {
+    it('detects linear scale with auto domain', () => {
         const result = isScaleWithDomain({ variant: 'linear', domain: 'auto' })
         expect(result).toBeFalsy()
     })
 
-    it('checks linear scale with part-auto domain', () => {
+    it('detects linear scale with part-auto domain', () => {
         const resultA = isScaleWithDomain({ variant: 'linear', domain: [0, 'auto'] })
         expect(resultA).toBeFalsy()
         const resultB = isScaleWithDomain({ variant: 'linear', domain: ['auto', 0] })
         expect(resultB).toBeFalsy()
     })
 
-    it('checks linear scale without domain', () => {
+    it('detects linear scale without domain', () => {
         const result = isScaleWithDomain({ variant: 'linear' })
         expect(result).toBeFalsy()
     })
