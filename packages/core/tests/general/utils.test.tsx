776e1cd4ac3d22cfe2c4d0a76c97293912df5365
tkonopka <tokonopka@gmail.com>
2023-06-23 09:46:23 +0100

    move tests to reflect code organization

--- packages/core/tests/general/utils.test.tsx
+++ packages/core/tests/general/utils.test.tsx
@@ -1,4 +1,67 @@
-import { getCenter, url, cloneProps, mergeProps } from '../../src/general/utils'
+import { getAnchoredOrigin, getCenter, url, cloneProps, mergeProps } from '../../src/general/utils'
+import { AnchorSpec, NumericPositionSpec, SizeSpec } from '../../src/general/types'
+
+describe('getAnchoredOrigin', () => {
+    it('computes origin - top-left position with top-left anchor', () => {
+        const inputs = {
+            position: [0, 0] as NumericPositionSpec,
+            size: [10, 20] as SizeSpec,
+            anchor: [0, 0] as AnchorSpec,
+        }
+        const result = getAnchoredOrigin(inputs.position, inputs.size, inputs.anchor)
+        expect(result).toEqual([0, 0])
+    })
+
+    it('computes origin - top-left position with top-middle anchor', () => {
+        const inputs = {
+            position: [0, 0] as NumericPositionSpec,
+            size: [10, 20] as SizeSpec,
+            anchor: [0.5, 0] as AnchorSpec,
+        }
+        const result = getAnchoredOrigin(inputs.position, inputs.size, inputs.anchor)
+        expect(result).toEqual([-5, 0])
+    })
+
+    it('computes origin - top-center with top-middle anchor', () => {
+        const inputs = {
+            position: [50, 0] as NumericPositionSpec,
+            size: [10, 20] as SizeSpec,
+            anchor: [0.5, 0] as AnchorSpec,
+        }
+        const result = getAnchoredOrigin(inputs.position, inputs.size, inputs.anchor)
+        expect(result).toEqual([45, 0])
+    })
+
+    it('computes origin - top-right with top-right anchor', () => {
+        const inputs = {
+            position: [100, 0] as NumericPositionSpec,
+            size: [10, 20] as SizeSpec,
+            anchor: [1, 0] as AnchorSpec,
+        }
+        const result = getAnchoredOrigin(inputs.position, inputs.size, inputs.anchor)
+        expect(result).toEqual([90, 0])
+    })
+
+    it('computes origin - bottom-right with bottom-right anchor', () => {
+        const inputs = {
+            position: [100, 200] as NumericPositionSpec,
+            size: [10, 20] as SizeSpec,
+            anchor: [1, 1] as AnchorSpec,
+        }
+        const result = getAnchoredOrigin(inputs.position, inputs.size, inputs.anchor)
+        expect(result).toEqual([90, 180])
+    })
+
+    it('computes origin - middle-right with middle-left anchor', () => {
+        const inputs = {
+            position: [100, 100] as NumericPositionSpec,
+            size: [10, 20] as SizeSpec,
+            anchor: [0, 0.5] as AnchorSpec,
+        }
+        const result = getAnchoredOrigin(inputs.position, inputs.size, inputs.anchor)
+        expect(result).toEqual([100, 90])
+    })
+})
 
 describe('getCenter', () => {
     it('computes center of an anchored box', () => {
